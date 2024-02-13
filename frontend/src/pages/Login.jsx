f8d6fc244467580340cb6bd171e23fb5bf564c03
Ababsa Rabii <rabiiababsa10@gmail.com>
2024-02-13 14:50:51 +0100

    Fix indentation and update database name

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { axiosClient } from "../api/axios";
 
+// Laravel breeze auth already installed
 // Use async arrowed function to make API calls
 // const csrf = await axiosClient.get('/sanctum/csrf-cookie')
 // const data = await axiosClient.post('/login', values)
